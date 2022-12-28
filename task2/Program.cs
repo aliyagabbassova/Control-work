d2ecd635a9688027c61c01a9aceb96e0d8ef9acb
aliya <aliya.gabbassova1911@gmail.com>
2022-12-28 14:19:59 +0600

    Задала 3 массива согласно примера

---
+++ task2/Program.cs
@@ -0,0 +1,6 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+string[] array1 = {"Hello", "2", "world", ":-)"};
+string[] array2 = {"1234", "1567", "-2", "computer science"};
+string[] array3 = {"Russia", "Denmark", "Kazan"};
+int maxlength = 3;
