977fb1ee0f60ec7b013599da40dbcc645912be90
aliya <aliya.gabbassova1911@gmail.com>
2022-12-28 14:23:07 +0600

    Создала метод чтобы задать нужную длину массива, внутри него цикл

--- task2/Program.cs
+++ task2/Program.cs
@@ -4,3 +4,10 @@ string[] array1 = {"Hello", "2", "world", ":-)"};
 string[] array2 = {"1234", "1567", "-2", "computer science"};
 string[] array3 = {"Russia", "Denmark", "Kazan"};
 int maxlength = 3;
+string[] ShortArray(string[] array)
+	{
+	int length = array.Length;			
+	string[] result = new string[length];
+	int count = 0;
+		for (int i = 0; i < length; i++)
+		{
